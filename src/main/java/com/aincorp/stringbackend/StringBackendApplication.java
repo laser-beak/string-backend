90e6633391cc6e4b29fd5a0be0f27a5c8653fe1d
laser-beak <laserbeak.des@gmail.com>
2019-05-04 01:12:11 +0300

    SB-1 Initial commit.

---
+++ src/main/java/com/aincorp/stringbackend/StringBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aincorp.stringbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StringBackendApplication.class, args);
+    }
+
+}

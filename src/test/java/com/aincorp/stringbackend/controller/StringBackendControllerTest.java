ae1e7742d4876df70ece3aca0a1ad20ca334baa5
laser-beak <laserbeak.des@gmail.com>
2019-11-19 23:13:03 +0300

    SB-2 Create test class for controller.

---
+++ src/test/java/com/aincorp/stringbackend/controller/StringBackendControllerTest.java
@@ -0,0 +1,33 @@
+package com.aincorp.stringbackend.controller;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+public class StringBackendControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void reverse() {
+    }
+
+    @Test
+    public void palindrome() {
+    }
+
+    @Test
+    public void substring() {
+    }
+}
